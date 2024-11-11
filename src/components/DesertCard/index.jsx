f1351507aca8231de83b9513a35f2f8fdf2e067f
Manannikov Kirill <113590303+themankov@users.noreply.github.com>
2024-11-11 23:50:16 +0300

    Исправление бага поиска

--- src/components/DesertCard/index.jsx
+++ src/components/DesertCard/index.jsx
@@ -1,24 +1,19 @@
 import style from './DesertCard.module.scss';
-import cake3 from '../../images/cake3.png';
 import { Link } from 'react-router-dom';
-const DesertCard = ({id,name,description,img}) => {
+const DesertCard = ({ id, name, description, img }) => {
   return (
     <Link to={`/${id}`}>
-      
-    <div className={style.dessert_item}>
-      <div className={style.image_container}>
-        <img src={img} alt={name} />
+      <div className={style.dessert_item}>
+        <div className={style.image_container}>
+          <img src={img} alt={name} />
+        </div>
+        <h3>{name}</h3>
+        <ul>
+          {description.map((item, index) => {
+            return <li key={id}>{`-${item}`}</li>;
+          })}
+        </ul>
       </div>
-      <h3>{name}</h3>
-      <ul>
-        {description.map((item,index)=>{
-        return(
-         <li key={id}>{`-${item}`}</li>
-        )
-        })}
-
-      </ul>
-    </div>
     </Link>
   );
 };
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,4 +1,3 @@
-
 // export async function getData(
 //   page,
 //   limit,
@@ -15,129 +14,137 @@
 //     alert(e);
 //   }
 // }
-import axios from 'axios';
 
 const API_URL = 'https://cloud-api.yandex.net/v1/disk';
-const ACCESS_TOKEN = 'y0_AgAAAABpLP7zAADLWwAAAAEXc6WeAADAQDH86zJKj5USSGa7XLLGVQ44zQ'; 
+const ACCESS_TOKEN =
+  'y0_AgAAAABpLP7zAADLWwAAAAEXc6WeAADAQDH86zJKj5USSGa7XLLGVQ44zQ';
 // y0_AgAAAABpLP7zAAy8NAAAAAEXcWkTAABHyry06WdP-6DnQ2jCJIF2iopVHQ
 // y0_AgAAAABpLP7zAADLWwAAAAEXc6WeAADAQDH86zJKj5USSGa7XLLGVQ44zQ
 
 export async function getFolderContents(folderPath) {
-    const url = `https://cloud-api.yandex.net/v1/disk/resources?path=${encodeURIComponent(folderPath)}`;
-    
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization': `OAuth ${ACCESS_TOKEN}`
-        }
-    });
+  const url = `https://cloud-api.yandex.net/v1/disk/resources?path=${encodeURIComponent(
+    folderPath
+  )}`;
 
-    if (response.ok) {
-        const data = await response.json();
-        
-        return data; // Возвращает информацию о папке и файлах в ней
-    } else {
-        console.error('Ошибка:', response.statusText);
-    }
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: `OAuth ${ACCESS_TOKEN}`,
+    },
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+
+    return data; // Возвращает информацию о папке и файлах в ней
+  } else {
+    console.error('Ошибка:', response.statusText);
+  }
 }
 
 export async function loadJsonFile(filePath) {
-   
+  const url = `https://cloud-api.yandex.net/v1/disk/resources/download?path=${encodeURIComponent(
+    filePath
+  )}`;
 
-    const url = `https://cloud-api.yandex.net/v1/disk/resources/download?path=${encodeURIComponent(filePath)}`;
-  
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization': `OAuth ${ACCESS_TOKEN}`
-        }
-    });
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: `OAuth ${ACCESS_TOKEN}`,
+    },
+  });
 
-    if (response.ok) {
+  if (response.ok) {
+    const data = await response.json();
+    const jsonUrl = data.href; // Получаем прямую ссылку на JSON-файл
+    const jsonResponse = await fetch(jsonUrl);
+    const jsonData = await jsonResponse.json();
 
-        const data = await response.json();
-        const jsonUrl = data.href; // Получаем прямую ссылку на JSON-файл
-        const jsonResponse = await fetch(jsonUrl);
-        const jsonData = await jsonResponse.json();
-        
-        return jsonData;
-    } else {
-        console.error('Ошибка при загрузке JSON:', response.statusText);
-    }
+    return jsonData;
+  } else {
+    console.error('Ошибка при загрузке JSON:', response.statusText);
+  }
 }
 // Функция для получения прямой ссылки на изображение
 async function getImageUrl(filePath) {
-    const url = `https://cloud-api.yandex.net/v1/disk/resources/download?path=${encodeURIComponent(filePath)}`;
+  const url = `https://cloud-api.yandex.net/v1/disk/resources/download?path=${encodeURIComponent(
+    filePath
+  )}`;
 
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization': `OAuth ${ACCESS_TOKEN}`
-        }
-    });
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: `OAuth ${ACCESS_TOKEN}`,
+    },
+  });
 
-    if (response.ok) {
-        const data = await response.json();
-        return data.href; 
-    } else {
-        console.error('Ошибка при получении ссылки на изображение:', response.statusText);
-    }
+  if (response.ok) {
+    const data = await response.json();
+    return data.href;
+  } else {
+    console.error(
+      'Ошибка при получении ссылки на изображение:',
+      response.statusText
+    );
+  }
 }
 
+export async function prepareDisplayData(
+  jsonFilePath,
+  page = 'main',
+  offset = 0,
+  input = '',
+  id = 0
+) {
+  let limit = 8;
+  let currentOffset = 0;
+  if (page === 'menu') {
+    currentOffset = offset;
+  }
 
-export async function prepareDisplayData(jsonFilePath,page='main', offset=0,input='',id=0) {
-    let limit = 8;
-    let currentOffset = 0;
-    if (page === "menu") {
-        currentOffset = offset;
-    }
+  const endOffset = currentOffset + limit;
+  const jsonData = await loadJsonFile(jsonFilePath);
+  const displayData = await Promise.all(
+    jsonData.map(async (item, index) => {
+      if (input) {
+        if (item.name.startsWith(input.trim())) {
+          const imageUrl = await getImageUrl(item.imagePath);
+          return {
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            imageUrl: imageUrl,
+          };
+        }
+        return null;
+      } else if (page === 'card') {
+        console.log(item.id);
+        if (String(item.id) === id) {
+          const imageUrl = await getImageUrl(item.imagePath);
+          return {
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            imageUrl: imageUrl,
+          };
+        }
+        return null;
+      } else {
+        if (index >= currentOffset && index < endOffset) {
+          const imageUrl = await getImageUrl(item.imagePath);
+          return {
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            imageUrl: imageUrl,
+          };
+        }
+        return null;
+      }
+    })
+  );
 
-    const endOffset = currentOffset + limit; 
-    const jsonData = await loadJsonFile(jsonFilePath);
-    const displayData = await Promise.all(
-        jsonData.map(async (item, index) => {
-            if(input){
-                if (item.name.startsWith(input.trim())) {
-                    const imageUrl = await getImageUrl(item.imagePath); 
-                    return {
-                        id: item.id,
-                        name: item.name,
-                        description: item.description,
-                        imageUrl: imageUrl,
-                    };
-                }
-                return null; 
-            }else if(page==='card'){
-                console.log(item.id);
-                if(String(item.id)===id){
-                    const imageUrl = await getImageUrl(item.imagePath); 
-                return {
-                    id: item.id,
-                    name: item.name,
-                    description: item.description,
-                    imageUrl: imageUrl,
-                };
-            }
-            return null; 
-
-            }else{
-                if (index >= currentOffset && index < endOffset) {
-                    const imageUrl = await getImageUrl(item.imagePath); 
-                    return {
-                        id: item.id,
-                        name: item.name,
-                        description: item.description,
-                        imageUrl: imageUrl,
-                    };
-                }
-                return null; 
-            }
-            
-            
-        })
-    );
-
-    const filteredData = displayData.filter(item => item !== null);
-    console.log('Filtered displayData:', filteredData);
-    return filteredData;
+  const filteredData = displayData.filter((item) => item !== null);
+  console.log('Filtered displayData:', filteredData);
+  return filteredData;
 }
